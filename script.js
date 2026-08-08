c0bd18b8ed38ee887bcf3fbfb13877c713a0471c
jgvkghv <deluxs4300@gmail.com>
2026-08-08 13:09:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// Инициализация Telegram WebApp
 const tg = window.Telegram.WebApp;
-tg.expand(); // Разворачиваем на весь экран
+tg.expand(); // Растягиваем мини-приложение на весь экран
 
-document.getElementById('save-btn').addEventListener('click', () => {
+document.getElementById('submit-btn').addEventListener('click', () => {
     const textValue = document.getElementById('emoji-text').value;
     const colorValue = document.getElementById('emoji-color').value;
 
-    // Упаковываем данные в JSON объект
-    const formData = {
+    // Упаковываем настройки в JSON
+    const payload = {
         text: textValue,
         fill: colorValue
     };
 
-    // Отправляем данные боту и закрываем Mini App
-    tg.sendData(JSON.stringify(formData));
+    // Передаем данные боту и закрываем мини-приложение
+    tg.sendData(JSON.stringify(payload));
 });
